f858fdb379805fc25e488a1fb7a7ec61dd0470b7
Roman Bagautdinov <rrbagautdinov@domclick.ru>
2022-05-31 23:44:45 +0300

    add capabilities for selenoid

--- src/test/java/com/demoqa/textBox/DemoqaTextBox.java
+++ src/test/java/com/demoqa/textBox/DemoqaTextBox.java
@@ -7,6 +7,7 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.nio.charset.StandardCharsets;
 
@@ -26,6 +27,11 @@ public class DemoqaTextBox {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Test
